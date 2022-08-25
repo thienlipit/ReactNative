5e7a93d85f6dbd889530918b46690ab07b19dffc
Tran Minh Thien <tranthienrs24@gmail.com>
2022-08-25 09:15:46 +0700

    upadate index.js

--- FirstApps/index.js
+++ FirstApps/index.js
@@ -8,4 +8,4 @@ import {name as appName} from './app.json';
 import {welcome, logIn, register} from './screens/index'
 
 // AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => register );
+AppRegistry.registerComponent(appName, () => welcome );
