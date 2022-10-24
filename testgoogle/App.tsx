20caaada681149f2376da955b289e508c977272d
Thien Tran Minh <tmthien@tma.com.vn>
2022-10-24 10:30:11 +0700

    upload image to firebase temp1

--- testgoogle/App.tsx
+++ testgoogle/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Text, View, Button } from 'react-native';
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Image,
+    Platform,
+    PermissionsAndroid,
+    Button,
+    Alert,
+    ScrollView,
+    StatusBar,
+    Dimensions,
+} from 'react-native';
+import {
+    CameraOptions,
+    ImageLibraryOptions,
+    launchCamera,
+    launchImageLibrary
+} from 'react-native-image-picker';
 import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
+import UploadScreen from './src/screens/UploadScreen';
+
+
 const App = () => {
+    // const { height, width } = Dimensions.get('screen');
+    // console.log(height, width);
     GoogleSignin.configure({
         webClientId:
             '1072095487828-m7panf3uan4gtej7s0ft4jcmc51i3vrr.apps.googleusercontent.com',
@@ -39,20 +64,242 @@ const App = () => {
 
     };
 
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
-            <Text> Hello world here ok men</Text>
-            <Button title="Google Sign-In" onPress={signInWithGoogleAsync} />
-            <Button title="Google Sign-Out" onPress={signOutWithGoogleAsync} />
+    //----------------------------------------------------------------
 
-            <GoogleSigninButton
-                style={{ width: 250, height: 48 }}
-                size={GoogleSigninButton.Size.Wide}
-                color={GoogleSigninButton.Color.Dark}
-                onPress={signInWithGoogleAsync}
-            />
-        </View>
+    const [filePath, setFilePath] = useState({});
+    const requestCameraPermission = async () => {
+        if (Platform.OS === 'android') {
+            try {
+                const granted = await PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.CAMERA,
+                    {
+                        title: 'Camera Permission',
+                        message: 'App needs camera permission',
+                    },
+                );
+                // If CAMERA Permission is granted
+                return granted === PermissionsAndroid.RESULTS.GRANTED;
+            } catch (err) {
+                console.warn(err);
+                return false;
+            }
+        } else {
+            return true;
+        }
+    };
+    const requestExternalWritePermission = async () => {
+        if (Platform.OS === 'android') {
+            try {
+                const granted = await PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+                    {
+                        title: 'External Storage Write Permission',
+                        message: 'App needs write permission',
+                    },
+                );
+                // If WRITE_EXTERNAL_STORAGE Permission is granted
+                return granted === PermissionsAndroid.RESULTS.GRANTED;
+            } catch (err) {
+                console.warn(err);
+                Alert.alert('Write permission err', err);
+            }
+            return false;
+        } else return true;
+    };
+
+    const captureImage = async (type: any) => {
+        let options: CameraOptions = {
+            mediaType: type,
+            maxWidth: 1920,
+            maxHeight: 1080,
+            quality: 1,
+            videoQuality: 'high',
+            durationLimit: 30, //Video max duration in seconds
+            saveToPhotos: true,
+            // includeBase64: true,
+        };
+        let isCameraPermitted = await requestCameraPermission();
+        let isStoragePermitted = await requestExternalWritePermission();
+        if (isCameraPermitted && isStoragePermitted) {
+            launchCamera(options, (response: any) => {
+                console.log('Response = ', response);
+
+                if (response.didCancel) {
+                    Alert.alert('User cancelled camera picker');
+                    return;
+                } else if (response.errorCode === 'camera_unavailable') {
+                    Alert.alert('Camera not available on device');
+                    return;
+                } else if (response.errorCode === 'permission') {
+                    Alert.alert('Permission not satisfied');
+                    return;
+                } else if (response.errorCode === 'others') {
+                    Alert.alert(response.errorMessage);
+                    return;
+                }
+                // console.log('base64 -> ', response.base64);
+                console.log('uri -> ', response.assets[0].uri);
+                console.log('width -> ', response.assets[0].width);
+                console.log('height -> ', response.assets[0].height);
+                console.log('fileSize -> ', response.assets[0].fileSize);
+                console.log('type -> ', response.assets[0].type);
+                console.log('fileName -> ', response.assets[0].fileName);
+                setFilePath(response);
+            });
+        }
+    };
+    const chooseFile = (type: any) => {
+        let options: ImageLibraryOptions = {
+            mediaType: type,
+            maxWidth: 1920,
+            maxHeight: 1080,
+            quality: 1,
+        };
+        launchImageLibrary(options, (response: any) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                Alert.alert('User cancelled camera picker');
+                return;
+            } else if (response.errorCode === 'camera_unavailable') {
+                Alert.alert('Camera not available on device');
+                return;
+            } else if (response.errorCode === 'permission') {
+                Alert.alert('Permission not satisfied');
+                return;
+            } else if (response.errorCode === 'others') {
+                Alert.alert(response.errorMessage);
+                return;
+            }
+            // console.log('base64 -> ', response.assets[0].base64);
+            console.log('uri -> ', response.assets[0].uri);
+            console.log('width -> ', response.assets[0].width);
+            console.log('height -> ', response.assets[0].height);
+            console.log('fileSize -> ', response.assets[0].fileSize);
+            console.log('type -> ', response.assets[0].type);
+            console.log('fileName -> ', response.assets[0].fileName);
+            // setFilePath(response);
+            setFilePath(response);
+        });
+    };
+
+    //----------------------------------------------------------------
+    const imageView = () => {
+        if (Object.keys(filePath).length !== 0) {
+            console.log('Co du lieu moi in', filePath);
+            return (
+                <View>
+                    <Image
+                        source={{ uri: filePath.assets[0].uri }}
+                        style={styles.imageStyle}
+                    />
+                    <Text style={styles.textStyle}>{filePath.assets[0].uri}</Text>
+                </View>
+            );
+        }
+    };
+
+    return (
+        <SafeAreaView >
+            <ScrollView >
+                <StatusBar barStyle="dark-content" />
+
+                <UploadScreen />
+
+                <Text style={styles.titleText}>
+                    Example of Image Picker in React Native
+                </Text>
+                <View style={styles.container}>
+                    <View>
+                        {imageView()}
+
+                    </View>
+                    {/* <Image
+                        source={{ uri: filePath }}
+                        style={styles.imageStyle}
+                    />
+                    <Text style={styles.textStyle}>{filePath}</Text> */}
+
+                    <TouchableOpacity
+                        activeOpacity={0.5}
+                        style={styles.buttonStyle}
+                        onPress={() => captureImage('photo')}>
+                        <Text style={styles.textStyle}>
+                            Launch Camera for Image
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        activeOpacity={0.5}
+                        style={styles.buttonStyle}
+                        onPress={() => captureImage('video')}>
+                        <Text style={styles.textStyle}>
+                            Launch Camera for Video
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        activeOpacity={0.5}
+                        style={styles.buttonStyle}
+                        onPress={() => chooseFile('photo')}>
+                        <Text style={styles.textStyle}>Choose Image</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        activeOpacity={0.5}
+                        style={styles.buttonStyle}
+                        onPress={() => chooseFile('video')}>
+                        <Text style={styles.textStyle}>Choose Video</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+                    <Text> Hello world here ok men</Text>
+                    <Button title="Google Sign-In" onPress={signInWithGoogleAsync} />
+                    <Button title="Google Sign-Out" onPress={signOutWithGoogleAsync} />
+
+                    <GoogleSigninButton
+                        style={{ width: 250, height: 48 }}
+                        size={GoogleSigninButton.Size.Wide}
+                        color={GoogleSigninButton.Color.Dark}
+                        onPress={signInWithGoogleAsync}
+                    />
+                </View>
+
+
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+    },
+    titleText: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingVertical: 20,
+    },
+    textStyle: {
+        padding: 10,
+        color: 'black',
+        textAlign: 'center',
+    },
+    buttonStyle: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 5,
+        marginVertical: 10,
+        width: 250,
+    },
+    imageStyle: {
+        width: 350,
+        height: 350,
+        margin: 5,
+        resizeMode: 'cover', //enum('cover', 'contain', 'stretch', 'repeat', 'center')
+    },
+});
---
+++ testgoogle/src/screens/UploadScreen.tsx
@@ -0,0 +1,164 @@
+import React, { useState } from 'react';
+import {
+    View,
+    SafeAreaView,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    Platform,
+    Alert,
+    Image
+} from 'react-native';
+import {
+    CameraOptions,
+    ImageLibraryOptions,
+    launchCamera,
+    launchImageLibrary
+} from 'react-native-image-picker';
+import storage from '@react-native-firebase/storage';
+// import * as Progress from 'react-native-progress';
+
+const UploadScreen = () => {
+    const [image, setImage] = useState(null);
+    const [uploading, setUploading] = useState(false);
+    const [transferred, setTransferred] = useState(0);
+
+    const selectImage = () => {
+        let options: ImageLibraryOptions = {
+            mediaType: 'mixed',
+        };
+        launchImageLibrary(options, async (response: any) => {
+            // console.log('Response = ', response);
+            if (response.didCancel) {
+                Alert.alert('User cancelled camera picker');
+                return;
+            } else if (response.errorCode === 'camera_unavailable') {
+                Alert.alert('Camera not available on device');
+                return;
+            } else if (response.errorCode === 'permission') {
+                Alert.alert('Permission not satisfied');
+                return;
+            } else if (response.errorCode === 'others') {
+                Alert.alert(response.errorMessage);
+                return;
+            }
+            // console.log('base64 -> ', response.assets[0].base64);
+            console.log('uri -> ', response.assets[0].uri);
+            console.log('width -> ', response.assets[0].width);
+            console.log('height -> ', response.assets[0].height);
+            console.log('fileSize -> ', response.assets[0].fileSize);
+            console.log('type -> ', response.assets[0].type);
+            console.log('fileName -> ', response.assets[0].fileName);
+            // setFilePath(response);
+            const source: any = { uri: response.assets[0].uri };
+            console.log('Print URI: ', source);
+
+            const fileName = response.assets[0].uri.split('/').pop();
+            const fileType = fileName.split('.').pop();
+            console.log(fileName, fileType);
+
+            setImage(source);
+
+            console.log('--------------------------------------------------------');
+
+            const result = await launchImageLibrary(options);
+            console.log(result);
+
+        });
+    };
+
+    const uploadImage = async () => {
+        const { uri }: any = image;
+        const filename = uri.substring(uri.lastIndexOf('/') + 1);
+        console.log('Print Name: ', filename);
+        const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
+        setUploading(true);
+        setTransferred(0);
+        const task = storage()
+            .ref('test11.jpg')
+            .putFile(uploadUri);
+        // set progress state
+        task.on('state_changed', snapshot => {
+            setTransferred(
+                Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 10000
+            );
+        });
+        try {
+            await task;
+        } catch (e) {
+            console.error(e);
+        }
+        setUploading(false);
+        Alert.alert(
+            'Photo uploaded!',
+            'Your photo has been uploaded to Firebase Cloud Storage!'
+        );
+        setImage(null);
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <TouchableOpacity style={styles.selectButton} onPress={selectImage}>
+                <Text style={styles.buttonText}>Pick an image</Text>
+            </TouchableOpacity>
+            <View style={styles.imageContainer}>
+                {image !== null ? (
+                    <Image source={{ uri: image.uri }} style={styles.imageBox} />
+                ) : null}
+                {uploading ? (
+                    <View style={styles.progressBarContainer}>
+                        {/* <Progress.Bar progress={transferred} width={300} /> */}
+                    </View>
+                ) : (
+                    <TouchableOpacity style={styles.uploadButton} onPress={uploadImage}>
+                        <Text style={styles.buttonText}>Upload image</Text>
+                    </TouchableOpacity>
+                )}
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default UploadScreen;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#bbded6',
+
+    },
+    selectButton: {
+        borderRadius: 5,
+        width: 150,
+        height: 50,
+        backgroundColor: '#8ac6d1',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    uploadButton: {
+        borderRadius: 5,
+        width: 150,
+        height: 50,
+        backgroundColor: '#ffb6b9',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    imageContainer: {
+        marginTop: 30,
+        marginBottom: 50,
+        alignItems: 'center'
+    },
+    progressBarContainer: {
+        marginTop: 20
+    },
+    imageBox: {
+        width: 300,
+        height: 300
+    }
+});
