f2b0dac5d04afd4ac18babc3fc4930c169dc36a4
Thien Tran Minh <tmthien@tma.com.vn>
2022-10-26 09:38:45 +0700

    add play sound demo

--- testgoogle/src/screens/documentPicker.tsx
+++ testgoogle/src/screens/documentPicker.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Alert, Button, Image, StyleSheet, Text, View, Dimensions } from 'react-native';
 import DocumentPicker, { DocumentPickerResponse } from 'react-native-document-picker';
 import ImageZoom from 'react-native-image-pan-zoom';
+import SoundPlayer from 'react-native-sound-player';
 
 const DocumentPickerEx = () => {
     const [imgResponse, setImgResponse] = useState<DocumentPickerResponse[] | undefined>();
@@ -20,9 +21,22 @@ const DocumentPickerEx = () => {
 
         } catch { }
     };
+    const playMp3 = () => {
+        try {
+            // play the file tone.mp3
+            SoundPlayer.playSoundFile('example', 'mp3');
+            // or play from url
+            // SoundPlayer.playUrl('https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_500KB_MP3.mp3');
+        } catch (e) {
+            console.log('cannot play the sound file', e);
+        }
+    };
 
     return (
         <View style={styles.constainer}>
+            <Text style={{ color: 'red' }}>Play MP3 file</Text>
+            <Button title='play' onPress={playMp3} />
+
             <Button title="Choose a file here" onPress={openPicker} />
 
             {imgResponse !== undefined ? (
@@ -30,7 +44,7 @@ const DocumentPickerEx = () => {
                     <ImageZoom cropWidth={300} cropHeight={300} imageWidth={300} imageHeight={300} >
                         <Image style={{ height: 300, width: 300, marginTop: 20 }} source={{ uri: imgResponse[0]?.uri }} />
                     </ImageZoom>
-                    
+
                     <Text style={{ color: '#fff' }}>{imgResponse[0].name}</Text>
                 </View>
             ) : undefined}
