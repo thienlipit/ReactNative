4fb8d406bb3e7a585992ae59f6e5e9b5bdf90464
Tran Minh Thien <tranthien@Trans-MacBook-Pro.local>
2022-10-27 22:01:29 +0700

    play sound not ok

--- ShopDunkApp/src/screens/sound/soundScreen.tsx
+++ ShopDunkApp/src/screens/sound/soundScreen.tsx
@@ -2,44 +2,105 @@ import * as React from 'react';
 import { Text, View, Image, Button, StyleSheet, ImageBackground, Pressable } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Slider from '@react-native-community/slider';
-import SoundPlayer from 'react-native-sound-player';
+import Sound from 'react-native-sound';
+
+let whoosh = new Sound('sample_12s.mp3', Sound.MAIN_BUNDLE, (error) => {
+    if (error) {
+        console.log('failed to load the sound', error);
+        return;
+    }
+    // loaded successfully
+    console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+});
 
 const SoundScreen = ({ navigation }: any) => {
-    const playSong = () => {
-        try {
-            console.log('playing');
-            // play the file tone.mp3
-            SoundPlayer.playSoundFile('example', 'mp3');
-            // or play from url
-            // SoundPlayer.playUrl('https://example.com/music.mp3');
-        } catch (e) {
-            console.log('cannot play the sound file', e);
-        }
-    };
+    Sound.setCategory("Playback")
+
+    const playEX = (songSelected: Sound) => {
+        console.log('clicked play')
+        // Play the sound with an onEnd callback
+        songSelected.play((success) => {
+            if (success) {
+                console.log('successfully finished playing');
+            } else {
+                console.log('playback failed due to audio decoding errors');
+            }
+        })
+    }
+    const stopEX = (songSelected: Sound) => {
+        console.log('clicked stop')
+        songSelected.stop()
+    }
+
+    const pauseEX = (songSelected: Sound) => {
+        console.log('clicked pause')
+        songSelected.pause()
+    }
+
+    const next15s = (songSelected: Sound) => {
+        songSelected.setCurrentTime(2)
+        songSelected.play()
+        songSelected.getCurrentTime((seconds) => {
+            console.log('at ' + seconds)
+            let newTime = seconds + 1
+            console.log('New time set:', newTime)
+            // var add15 = songSelected.setCurrentTime(newTime)
+            console.log('at11 ' + seconds)
+            // songSelected.getCurrentTime
+            // console.log('+3second ', songSelected.getCurrentTime((seconds) => {
+            // console.log('+3s ', seconds)
+            // songSelected.play()
+            // }))
+        });
+        // songSelected.setCurrentTime(songSelected.getCurrentTime + 15 )
+    }
+
+    const prev15s = (songSelected: Sound) => {
+
+    }
+
+
     return (
         <View style={{ justifyContent: 'center', alignItems: 'center' }}>
             <Text style={{ fontSize: 20, color: 'red' }}>SoundScreen Here</Text>
 
-            <Slider
-                style={{ width: 200, height: 40 }}
-                minimumValue={0}
-                maximumValue={1}
-                minimumTrackTintColor="#FFFFFF"
-                maximumTrackTintColor="#000000"
-            />
+            <View style={{ flexDirection: 'row', marginVertical: 10, justifyContent: 'space-between' }}>
+                <Text style={{ color: 'black' }}>0:00</Text>
+                <Slider
+                    style={{ width: 200, height: 40 }}
+                    minimumValue={0}
+                    maximumValue={1}
+                    minimumTrackTintColor="#FFFFFF"
+                    maximumTrackTintColor="#000000"
+                    value={whoosh.getCurrentTime((seconds) => {
+                        return seconds
+                    })}
+                />
+                <Text style={{ color: 'black' }}>{whoosh.getDuration()}</Text>
+            </View>
 
-            {/* <Button title='Test Play' onPress={playSong()} /> */}
 
 
-            <View style={{ flexDirection: 'row' }}>
-                <Icon style={{ marginRight: 10 }} name="backward" size={30} color="#900" />
+            {/* <Button title='Press to Play' onPress={() => playEX(whoosh)} /> */}
+            {/* <Button title='Test Stop' onPress={getInfo()} /> */}
 
-                {/* <Pressable onPress={playSong()}> */}
-                <Icon style={{ marginRight: 10 }} name="play" size={30} color="#900" />
-                {/* </Pressable> */}
 
-                <Icon style={{ marginRight: 10 }} name="pause" size={30} color="#900" />
-                <Icon style={{ marginRight: 10 }} name="forward" size={30} color="#900" />
+            <View style={{ flexDirection: 'row', marginVertical: 20, justifyContent: 'space-between' }}>
+                <Pressable onPress={() => prev15s(whoosh)} >
+                    <Icon style={{ marginRight: 15 }} name="backward" size={30} color="#900" />
+                </Pressable>
+
+                <Pressable onPress={() => playEX(whoosh)}>
+                    <Icon style={{ marginRight: 15 }} name="play" size={30} color="#900" />
+                </Pressable>
+
+                <Pressable onPress={() => pauseEX(whoosh)}>
+                    <Icon style={{ marginRight: 15 }} name="pause" size={30} color="#900" />
+                </Pressable>
+
+                <Pressable onPress={() => next15s(whoosh)} >
+                    <Icon name="forward" size={30} color="#900" />
+                </Pressable>
             </View>
 
 
