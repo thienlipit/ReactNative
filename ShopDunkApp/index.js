169c94da207e9d587e834ad064104c713dce6840
Thien Tran Minh <tmthien@tma.com.vn>
2022-10-27 11:45:25 +0700

    play mp3

--- ShopDunkApp/index.js
+++ ShopDunkApp/index.js
@@ -7,5 +7,7 @@ import { AppRegistry } from 'react-native';
 import MyApp from './src/navigators/bottomNavigationBar';
 
 import { name as appName } from './app.json';
+import TrackPlayer from 'react-native-track-player';
 
 AppRegistry.registerComponent(appName, () => MyApp);
+TrackPlayer.registerPlaybackService(() => require('./src/screens/sound/TrackPlayerSerivce'));
--- ShopDunkApp/src/screens/sound/soundScreen.tsx
+++ ShopDunkApp/src/screens/sound/soundScreen.tsx
@@ -1,22 +1,60 @@
 import * as React from 'react';
-import { Text, View, Image, Button, StyleSheet, ImageBackground, } from 'react-native';
+import { Text, View, Image, Button, StyleSheet, ImageBackground, Pressable } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Slider from '@react-native-community/slider';
+import SoundPlayer from 'react-native-sound-player';
 
 const SoundScreen = ({ navigation }: any) => {
+    const playSong = () => {
+        try {
+            console.log('playing');
+            // play the file tone.mp3
+            SoundPlayer.playSoundFile('example', 'mp3');
+            // or play from url
+            // SoundPlayer.playUrl('https://example.com/music.mp3');
+        } catch (e) {
+            console.log('cannot play the sound file', e);
+        }
+    };
     return (
         <View style={{ justifyContent: 'center', alignItems: 'center' }}>
             <Text style={{ fontSize: 20, color: 'red' }}>SoundScreen Here</Text>
 
+            <Slider
+                style={{ width: 200, height: 40 }}
+                minimumValue={0}
+                maximumValue={1}
+                minimumTrackTintColor="#FFFFFF"
+                maximumTrackTintColor="#000000"
+            />
+
+            {/* <Button title='Test Play' onPress={playSong()} /> */}
+
+
+            <View style={{ flexDirection: 'row' }}>
+                <Icon style={{ marginRight: 10 }} name="backward" size={30} color="#900" />
+
+                {/* <Pressable onPress={playSong()}> */}
+                <Icon style={{ marginRight: 10 }} name="play" size={30} color="#900" />
+                {/* </Pressable> */}
+
+                <Icon style={{ marginRight: 10 }} name="pause" size={30} color="#900" />
+                <Icon style={{ marginRight: 10 }} name="forward" size={30} color="#900" />
+            </View>
+
+
             <Button title='Sound Move to Iphone screen' onPress={() => navigation.navigate('Iphone')} />
 
+            {/* <Icon name="ios-person" size={30} color="#4F8EF7" /> */}
 
 
-            <Image
-                style={styles.tinyLogo}
-                source={{
-                    uri: 'https://reactnative.dev/img/tiny_logo.png',
-                }}
-
-            />
+            <Icon.Button
+                name="facebook"
+                backgroundColor="#3b5998"
+            // onPress={loginWithFacebook}
+            >
+                Login with Facebook
+            </Icon.Button>
 
         </View>
     );
