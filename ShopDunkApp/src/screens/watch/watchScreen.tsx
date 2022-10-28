d5acb1664f95a2310cd7f3d33a12337b32e595aa
Thien Tran Minh <tmthien@tma.com.vn>
2022-10-28 11:05:17 +0700

    play video in watchScreen

--- ShopDunkApp/src/screens/watch/watchScreen.tsx
+++ ShopDunkApp/src/screens/watch/watchScreen.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Text, View, Image, Button, } from 'react-native';
+import VideoPlayer from 'react-native-video-player';
 
 const WatchScreen = ({ navigation }: any) => {
     return (
@@ -8,8 +9,21 @@ const WatchScreen = ({ navigation }: any) => {
 
             <Button title='Watch screen move to Home screen' onPress={() => navigation.navigate('Iphone')} />
 
+            <VideoPlayer
+                video={{ uri: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3' }}
+                videoWidth={1600}
+                videoHeight={300}
+                thumbnail={{ uri: 'https://img.thuthuatphanmem.vn/uploads/2018/10/09/anh-dep-nhat-the-gioi-ve-thien-nhien_041753462.jpg' }}
+            />
         </View>
     );
 };
 
 export default WatchScreen;
+
+{/* <VideoPlayer
+    video={{ uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' }}
+    videoWidth={1600}
+    videoHeight={900}
+    thumbnail={{ uri: 'https://img.thuthuatphanmem.vn/uploads/2018/10/09/anh-dep-nhat-the-gioi-ve-thien-nhien_041753462.jpg' }}
+/> */}
