fd055c19111ff82bf0e2a4099b3fc9af62a85b43
Thien Tran Minh <tmthien@tma.com.vn>
2022-10-31 09:58:21 +0700

    run mp4 file in cache ok

--- MusicPlayer/App.tsx
+++ MusicPlayer/App.tsx
@@ -1,89 +1,45 @@
-import React, { type PropsWithChildren } from 'react';
+import React, { useState } from 'react';
 import {
     SafeAreaView,
     ScrollView,
     StatusBar,
     StyleSheet,
     Text,
-    useColorScheme,
     View,
+    Button,
+    Alert,
 } from 'react-native';
-
-import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-    PropsWithChildren<{
-        title: string;
-    }>
-> = ({ children, title }) => {
-    const isDarkMode = useColorScheme() === 'dark';
-    return (
-        <View style={styles.sectionContainer}>
-            <Text
-                style={[
-                    styles.sectionTitle,
-                    {
-                        color: isDarkMode ? Colors.white : Colors.black,
-                    },
-                ]}>
-                {title}
-            </Text>
-            <Text
-                style={[
-                    styles.sectionDescription,
-                    {
-                        color: isDarkMode ? Colors.light : Colors.dark,
-                    },
-                ]}>
-                {children}
-            </Text>
-        </View>
-    );
-};
+import AudioRecorderPlayer from 'react-native-audio-recorder-player';
 
 const App = () => {
-    const isDarkMode = useColorScheme() === 'dark';
+    const [state, setState] = useState({});
+    const audioRecorderPlayer = new AudioRecorderPlayer();
 
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    const onStartPlay = async () => {
+        console.log('onStartPlay');
+        const msg = await audioRecorderPlayer.startPlayer();
+        console.log(msg);
+        audioRecorderPlayer.addPlayBackListener((e) => {
+            setState({
+                currentPositionSec: e.currentPosition,
+                currentDurationSec: e.duration,
+                playTime: audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)),
+                duration: audioRecorderPlayer.mmssss(Math.floor(e.duration)),
+            });
+            return;
+        });
+    };
+
+    const onStopPlay = async () => {
+        console.log('onStopPlay');
+        audioRecorderPlayer.stopPlayer();
+        audioRecorderPlayer.removePlayBackListener();
     };
 
     return (
-        <SafeAreaView style={backgroundStyle}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                backgroundColor={backgroundStyle.backgroundColor}
-            />
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic"
-                style={backgroundStyle}>
-                <Header />
-                <View
-                    style={{
-                        backgroundColor: isDarkMode ? Colors.black : Colors.white,
-                    }}>
-                    <Section title="Step One">
-                        Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                        screen and then come back to see your edits.
-                    </Section>
-                    <Section title="See Your Changes">
-                        <ReloadInstructions />
-                    </Section>
-                    <Section title="Debug">
-                        <DebugInstructions />
-                    </Section>
-                    <Section title="Learn More">
-                        Read the docs to discover what to do next:
-                    </Section>
-                    <LearnMoreLinks />
-                </View>
-            </ScrollView>
+        <SafeAreaView>
+            <Text>TEST</Text>
+            <Button onPress={() => onStartPlay()} title="Play" />
         </SafeAreaView>
     );
 };
