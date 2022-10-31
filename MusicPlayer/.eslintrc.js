00810f52342419a964771f7f06e157a40e13ea35
Thien Tran Minh <tmthien@tma.com.vn>
2022-10-31 09:17:43 +0700

    add eslint

--- MusicPlayer/.eslintrc.js
+++ MusicPlayer/.eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
   root: true,
   extends: '@react-native-community',
@@ -10,6 +11,12 @@ module.exports = {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
+        'prettier/prettier': 'off',
+        'eol-last': 'off',
+        'react-native/no-inline-styles': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'no-trailing-spaces': 'off',
+        'comma-dangle': 'off',
       },
     },
   ],
