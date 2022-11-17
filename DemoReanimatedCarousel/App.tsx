adb066a4c58e4ab17920a8dd4f790dfdc07754c4
Tran Minh Thien <tranthien@Trans-MacBook-Pro.local>
2022-11-17 09:51:57 +0700

    init reanimated carousel project

---
+++ DemoReanimatedCarousel/App.tsx
@@ -0,0 +1,20 @@
+import React, { type PropsWithChildren } from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+const App = () => {
+    return (
+        <SafeAreaView style={{ flex: 1 }} >
+            <Text>ok men</Text>
+        </SafeAreaView >
+    );
+};
+
+export default App;
