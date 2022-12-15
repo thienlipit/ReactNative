acbce4b6165ed39315bb2fe0d5281867fb34c867
Tran Minh Thien <tranthienrs24@gmail.com>
2022-12-15 16:23:27 +0700

    demo fetch data online

--- IosFirstApp/App.tsx
+++ IosFirstApp/App.tsx
@@ -1,120 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, { useEffect, useState } from 'react';
+import { Button, Image, SafeAreaView, Text, ActivityIndicator, FlatList, View, } from 'react-native';
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+interface Movies {
+    id: string,
+    title: string,
+    releaseYear: string
+}
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const getMovies = async () => {
+        try {
+            const response = await fetch('https://reactnative.dev/movies.json');
+            const json = await response.json();
+            setData(json.movies);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    useEffect(() => {
+        getMovies();
+    }, []);
+    return (
+        <View style={{ flex: 1, padding: 24, backgroundColor: 'white' }}>
+            {isLoading ? <ActivityIndicator /> : (
+                <FlatList
+                    data={data}
+                    keyExtractor={({ id }, index) => id}
+                    renderItem={({ item }: { item: Movies }) => (
+                        <Text>{item.id}: {item.title}, {item.releaseYear}</Text>
+                    )}
+                />
+
+            )}
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+        // <SafeAreaView style={{ flex: 1, backgroundColor: '#e5e5e5', justifyContent: 'center', alignItems: 'center' }}>
+        //     <Text style={{ color: 'white', fontSize: 22, fontWeight: 'bold' }}>Ba vo mac cuoi qua :v</Text>
+        //     <Button title="Click me" />
+        //     <Image
+        //         style={{ height: 300, width: 300 }}
+        //         source={{
+        //             uri: 'https://tophinhanhdep.com/wp-content/uploads/2021/10/4K-Quality-Wallpapers.jpg',
+        //         }}
+        //     />
+        // </SafeAreaView>
+    );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
